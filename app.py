a17fc7be097f64c38a160ea3c803921e29df058b
PythonDot64 <jeff@opengpt.com>
2025-03-09 14:13:47 -0700

    version 0.1

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from cs50 import SQL
 
-from flask import Flask, request, render_template, session, redirect
+from flask import Flask, request, render_template, session, redirect, Response
 from flask_session import Session
 
 from helper import login_required, apology
@@ -28,24 +28,65 @@ def after_request(response):
 
 @app.route("/")
 @login_required
-def index():
+def index() -> tuple[str, int] | str:
     return render_template("index.html")
 
 
 @app.route("/login", methods=["GET", "POST"])
-def login():
+def login() -> tuple[str, int] | Response:
     session.clear()
     if request.method == "GET":
         return render_template("login.html")
+    
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    if not username:
+        return apology("Missing username")
+    
+    if not password:
+        return apology("Missing password")
+    
+    user = db.execute("SELECT id, password FROM user WHERE username=?")
+
+    if not check_password_hash(user[0]["password"], password):
+        return apology("Incorrect password")
+    
+    session["user_id"] = user[0]["id"]
+
+    return redirect('/')
 
 @app.route("/register", methods=["GET", "POST"])
-def register():
+def register() -> tuple[str, int] | Response:
     session.clear()
     if request.method == "GET":
         return render_template("register.html")
+    
     username = request.form.get("username")
+    password = request.form.get("password")
+
+    users = db.execute("SELECT * FROM user")
+
+    if not username:
+        return apology("Missing username")
+    
+    for user in users:
+        if user["username"] == username:
+            return apology("Username taken")
+        
+    if not password:
+        return apology("Missing password")
+    
+    if password != request.form.get("confirm"):
+        return apology("Confirmation and password don't match")
+    
+    db.execute("INSERT INTO user (username, password) VALUES(?,?)", username, generate_password_hash(password))
+
+    session["user_id"] = db.execute("SELECT id FROM user WHERE username=?", username)[0]["id"]
+
+    return redirect("/")
     
 
-def logout():
+def logout() -> tuple[str, int] | Response:
     session.clear()
     return redirect("/login")
