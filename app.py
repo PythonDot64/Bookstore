c9c884f3093a7b87c3d4705216157db47843b276
PythonDot64 <jeff@opengpt.com>
2025-03-10 19:43:38 -0700

    version 0.3 --added basic searching functionality

--- app.py
+++ app.py
@@ -3,18 +3,19 @@ from cs50 import SQL
 from darkdetect import isDark
 from flask import Flask, request, render_template, session, redirect, Response
 from flask_session import Session
+import requests
 
 from helper import login_required, apology
 
 from werkzeug.security import check_password_hash, generate_password_hash
 
-app = Flask(__name__)
+app: Flask = Flask(__name__)
 
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-db = SQL("sqlite:///bookstore.db")
+db: SQL = SQL("sqlite:///bookstore.db")
 
 
 # from finance in pset 10
@@ -30,9 +31,6 @@ def after_request(response):
 @app.route("/")
 @login_required
 def index() -> tuple[str, int] | str:
-    if "GET" == request.method:
-        return render_template("index.html")
-    
     return render_template("index.html")
 
 
@@ -42,8 +40,8 @@ def login() -> tuple[str, int] | Response:
     if request.method == "GET":
         return render_template("login.html")
     
-    username = request.form.get("username")
-    password = request.form.get("password")
+    username: str = request.form.get("username")
+    password: str = request.form.get("password")
 
     if not username:
         return apology("Missing username")
@@ -51,7 +49,7 @@ def login() -> tuple[str, int] | Response:
     if not password:
         return apology("Missing password")
     
-    user = db.execute("SELECT id, password FROM user WHERE username=?", username)
+    user: list[dict[str, int]] = db.execute("SELECT id, password FROM user WHERE username=?", username)
 
     if len(user) < 1:
         return apology("User not does not exist")
@@ -70,10 +68,10 @@ def register() -> tuple[str, int] | Response:
     if request.method == "GET":
         return render_template("register.html")
     
-    username = request.form.get("username")
-    password = request.form.get("password")
+    username: str = request.form.get("username")
+    password: str = request.form.get("password")
 
-    users = db.execute("SELECT * FROM user")
+    users: list[dict[str, int | str, str]] = db.execute("SELECT * FROM user")
 
     if not username:
         return apology("Missing username")
@@ -99,3 +97,15 @@ def register() -> tuple[str, int] | Response:
 def logout() -> tuple[str, int] | Response:
     session.clear()
     return redirect("/login")
+
+
+@app.route("/search")
+@login_required
+def search() -> tuple[str, int] | Response:
+    search: str = request.args.get("q")
+    print(search)
+    
+    url: str = f"https://openlibrary.org/search.json?q={search}"
+
+    response: Response = requests.get(url)
+    return render_template("search.html", books=response.json()["docs"])
